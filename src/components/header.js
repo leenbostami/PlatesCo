8220cbcc81bea46dbfe0da3f4520d3ac179661e7
LeenBostami <leenbostami@gmail.com>
2023-08-24 18:57:13 +0300

    fix css

--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,6 @@ export default function Header(props) {
   
   return (
     <div className="header">
-      <h1>{props.title}</h1>
-      <p>{props.subtitle}</p>
       <img src={logo} alt='logo' className="logo"></img>
       <ul className="header-items">
         <li>Home</li>
